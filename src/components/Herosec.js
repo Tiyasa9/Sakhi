174153abd6b6831dc611b2a083f89fc703d028c0
shrutikarathi6 <122254552+shrutikarathi6@users.noreply.github.com>
2024-04-10 02:39:38 +0530

    Css Changed
    Css Changed

--- src/components/Herosec.js
+++ src/components/Herosec.js
@@ -8,37 +8,36 @@ const Herosec = () => {
     return(
         <div>
             <Navbar />
-
+            <div className='Helplinecode'>
             <span className='helpline'>
                 Contact us 24/7  1-800-72544-38
             </span>
+            </div>
+        <div className='Herosection'>
 
-            <div className='hs'>
-
-            <div className='container1'>
-               <div className='container1-text'>
-                Domestic violence affects 30% of women in India.<br />
-                If you or someone you know is experiencing abuse, <br />
-                know that help is available. You are not alone, and <br />
-                there&#39;s nothing to be afraid of. Your safety and <br />
-                well-being are our priority, and all information shared <br />
-                will be kept strictly confidential.
-                </div>
-
-                <div className='container1-buttons'>
-                        <button>Get Help Now</button>
-                        <button>Learn more</button>
+            <div class='hs'>
+                <div class='container1'>
+                    <div class='container1-text'>
+                    Domestic violence affects 30% of women in India.<br />
+                    If you or someone you know is experiencing abuse, <br />
+                    know that help is available. You are not alone, and <br />
+                    there's nothing to be afraid of. Your safety and <br />
+                    well-being are our priority, and all information shared <br />
+                    will be kept strictly confidential.
                     </div>
-                
+
+                    <div class='container1-buttons'>
+                    <button>Get Help Now</button>
+                    <button className='Learnmore'>Learn more</button>
+                    </div>
                 </div>
 
-                <div>
-                    <img src= {pot} alt='potrait1' className='container1-img'></img>
+                <div className='girlimage'>
+                    <img src= {pot} alt='portrait1' class='container1-img'></img>
                 </div>
-
-
             </div>
 
+        </div>
             
             
         </div>
--- src/components/Mission.js
+++ src/components/Mission.js
@@ -9,22 +9,31 @@ const Mission = () => {
         <div className="homepage">
         <Features />
 
+
+        <div className="featurepage">
+
         <img src={woman} alt="womanPic" className="missionPic"></img>
-        <p>Providing holistic support and resources for survivors of domestic violence,
+        <div className="textside">
+        <p className="textside1">Providing holistic support and resources for survivors of domestic violence,
             ensuring confidentiality, empowerment, and access to essential services.</p>
         
-        <div className="mission">
-            <p>Our mission is to empower survivors of domestic violence by 
+       
+        <p className="textside2">Our mission is to empower survivors of domestic violence by 
                 providing access to critical support services, resources, and information 
                 in a safe and user-friendly online environment. We believe in the 
                 importance of raising awareness, promoting education, and fostering 
                 community collaboration to address the root causes of domestic violence 
                 and create a future free from abuse.
-            </p>
-            <button>Learn more</button>
+        </p>
+        {/* <div class='container1-buttons'> */}
+                    <button className="LLL">Learn More</button>
+                    {/* <button className='Learnmore'>Learn more</button> */}
+                    </div>
+        {/* </div> */}
+           
+   
         </div>
         </div>
-
     )
 }
 
